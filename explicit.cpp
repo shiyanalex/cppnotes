4545ccb41a11a1dc1f52b1bf6f1b5d3f797f19f8
Aleksandr <shiyanalex89@gmail.com>
2023-06-03 18:57:51 +0300

    1

--- explicit.cpp
+++ explicit.cpp
@@ -22,16 +22,18 @@ private:
 public:
     Coordiantes(int x, int y) : longitude(x), latitude(y) {}
 
-    loc_check() {
-        return std::douple<int, int> (longitude),
+    loc_check(Coordiantes&) {
+        return std::pair<longitude, latitude>;
     }
 }
 
 template <typename S>
 void someFunc(const S& s) {
+
 }
 
 int main(int, char**) {
+    /*
     Simple s3 = 11;
     // SimpleExplicit se3 = 11; - COMPILE ERROR
     SimpleExplicit se3 = SimpleExplicit(11);
@@ -39,6 +41,11 @@ int main(int, char**) {
     someFunc<Simple>(11);
     // someFunc<SimpleExplicit>(11); - COMPILE ERROR
     someFunc<SimpleExplicit>(SimpleExplicit(11));
+
+    */
+
+    Coordiantes c (11, 5);
+    Coordiantes::loc_check(c);
 
     return 0;
 }
